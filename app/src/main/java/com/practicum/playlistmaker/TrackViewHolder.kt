8e3b2da2760625ca21d983c26b3a958052ca5903
Минашкин Александр <horider@yandex.ru>
2023-09-04 22:00:41 +0300

    Доработки по комментариям ревьюера

--- app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -24,6 +25,9 @@ class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
             .placeholder(R.drawable.ic_placeholder)
             .centerCrop()
             .transform(RoundedCorners(2))
+            .diskCacheStrategy(DiskCacheStrategy.NONE)  // Отключение кеширования на диске
+            .skipMemoryCache(true)  // Отключение кеширования в памяти
             .into(albumImage)
     }
+
 }
